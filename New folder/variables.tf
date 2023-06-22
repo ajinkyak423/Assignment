610ed2c39575040fe69b6453b7a79dc4db273e8d
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2023-06-22 17:55:22 +0530

    Delete New folder directory

--- New folder/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "common_tags" {
-  default     = "Terraform"
-  description = "Common tags used in tf files"
-}
-
-variable "aws-availability_zone" {
-  default     = "us-east-1a"
-  description = "AWS availability_zone"
-}
