75facb169f9f038398175104eec34369d9c2ec2b
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2023-06-22 17:55:49 +0530

    Add files via upload

---
+++ code/variables.tf
@@ -0,0 +1,9 @@
+variable "common_tags" {
+  default     = "Terraform"
+  description = "Common tags used in tf files"
+}
+
+variable "aws-availability_zone" {
+  default     = "us-east-1a"
+  description = "AWS availability_zone"
+}
